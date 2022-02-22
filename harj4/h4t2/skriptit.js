0ecac932128d7e9836c679f7a82f6f9493ddd36b
Tommi Hulkkonne <ab9011@student.jamk.fi>
2022-02-22 10:01:11 +0200

    harj 4

--- harj4/h4t2/skriptit.js
+++ harj4/h4t2/skriptit.js
@@ -43,34 +43,28 @@ async function renderHouses() {
         housecontainer.className = 'houseContainer';
 
         let image = document.createElement('img');
-        image.src = filtered.image;
+        image.src = house.image;
         image.className = 'houseImage';
 
         let header = document.createElement('p');
         header.className = 'header';
-        header.innerHTML = filtered.address;
+        header.innerHTML = house.address;
 
         let koko = document.createElement('p');
         koko.className = 'size';
-        koko.innerHTML = filtered.size + " m2";
+        koko.innerHTML = house.size + " m2";
 
         let kuvaus = document.createElement('p');
         kuvaus.className = 'text';
-        kuvaus.innerHTML = filtered.text;
+        kuvaus.innerHTML = house.text;
 
         let hinta = document.createElement('p');
         hinta.className = 'price';
-        let numberstr = new Intl.NumberFormat('fi-FI').format(filtered.price);
+        let numberstr = new Intl.NumberFormat('fi-FI').format(house.price);
         hinta.innerHTML = numberstr + " euroa";
 
 
-       /*
-             Ohjelmoi tähän toiminnallisuus, jossa luodaan p-elementit
-             myös talon koon, tekstikuvauksen ja hinnan näyttämiselle
-             Hinnan voit muotoilla tuhaterottimia käyttävään suomalaiseen
-             muotoon esim:
-          let numberstr = new Intl.NumberFormat('fi-FI').format(house.price);
-        */
+
 
         housecontainer.appendChild(image);
         housecontainer.appendChild(header);
@@ -78,9 +72,6 @@ async function renderHouses() {
         housecontainer.appendChild(kuvaus);
         housecontainer.appendChild(hinta);
 
-                /* Ohjelmoi tähän toiminnallisuus, jossa liität edellä
-                    luomasi p-elementit myös housecontaineriin
-                */
 
 
         housediv.appendChild(housecontainer); 
